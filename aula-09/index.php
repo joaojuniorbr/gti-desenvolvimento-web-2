5eee8c9e1472dfdcaa8fe7c777ac577f1ff5218e
João Junior <negrelis@gmail.com>
2025-05-31 14:15:31 -0300

    :recycle: (aula 9) adicao de um item no menu e troca da imagem inicial

--- aula-09/index.php
+++ aula-09/index.php
@@ -11,7 +11,7 @@ include '../components/header.php';
 
   <h1 class="text-center text-2xl font-bold mb-10">Aula 09 - Linguagem PHP: Cookies, Sessions, e Autenticação de Usuários</h1>
 
-  <img src="../assets/images/pixeltrue-plan-1.svg" class="mx-auto h-auto max-w-sm mb-10 w-full" />
+  <img src="../assets/images/pixeltrue-healthy-eating-1.svg" class="mx-auto h-auto max-w-sm mb-10 w-full" />
 
   <?php include '../components/menu.php' ?>
 
