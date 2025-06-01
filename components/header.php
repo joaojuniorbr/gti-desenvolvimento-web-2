86b1dc045a7a43a864411666413690fb3d7f54da
João Junior <negrelis@gmail.com>
2025-05-31 21:13:57 -0300

    :chart_with_upwards_trend: (analytics) adicionado analytics ao projeto

--- components/header.php
+++ components/header.php
@@ -47,6 +47,19 @@
       @apply font-sans;
     }
   </style>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-64SHZV0V1E"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-64SHZV0V1E');
+  </script>
 </head>
 
 <body>
