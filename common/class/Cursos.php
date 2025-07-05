ce985a6a05322c97321151f6ba88378fb0d766ca
viniciusAhenrique <andradevinihenrique@gmail.com>
2025-07-05 16:29:51 -0300

    Busca de Cursos

--- common/class/Cursos.php
+++ common/class/Cursos.php
@@ -1,14 +1,19 @@
-<?php 
-class Cursos{
+<?php
+class Cursos
+{
   private mysqli $conn;
 
   public function __construct(mysqli $conn)
   {
     $this->conn = $conn;
   }
-    
-  public function listarCursos(){
-    $stmt = $this->conn->prepare("SELECT * FROM cursos ORDER BY title ASC");
+
+  public function listarCursos($limit)
+  {
+    if ($limit) {
+      $limit = ' LIMIT ' . $limit;
+    }
+    $stmt = $this->conn->prepare("SELECT * FROM cursos ORDER BY title ASC".$limit);
     $stmt->execute();
 
     $result = $stmt->get_result();
@@ -18,6 +23,20 @@ class Cursos{
       $data[] = $row;
     }
 
+    return $data;
+  }
+  public function buscarCursos($search)
+  {
+    $search = "%{$search}%";
+    $stmt = $this->conn->prepare("SELECT * FROM cursos WHERE title LIKE ? OR description LIKE ? ORDER BY title ASC");
+    $stmt->bind_param("ss", $search, $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = [];
+
+    while ($row = $result->fetch_object()) {
+      $data[] = $row;
+    }
     return $data;
   }
 }
